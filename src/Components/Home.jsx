1077df2c54348eec0464d97952acb0e18a6c2c12
ashish nagar <ashishnagar033@gmail.com>
2023-01-06 10:59:17 +0530

    categories added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,29 +12,47 @@ import SingleProduct from './SingleProduct'
 
 const Home = () => {
 
-  const { cartProducts , setCartProducts, isDarkMode, products , setProducts, searchData, showModel, setShowModel, modelProduct, setModelProduct } =  useContext(CartData)
   
-  let [fetching, setFatching] = useState(false)
-
+  const [fetching, setFatching] = useState(false)
+  
+  const [ categories ] = useState(["All","electronics","men's clothing" ,"women's clothing"])
+  
+  const [currentCategory, setCurrentCategory] = useState("All")
+  
+  const { cartProducts ,setProducts , setCartProducts, isDarkMode, products , searchData, showModel, setShowModel, modelProduct, setModelProduct } =  useContext(CartData)
+ 
   useEffect(() => {
     let productsApiUrl  = 'https://fakestoreapi.com/products'
-    
+     
+
     async function getProducts(productsApiUrl){
       setFatching(true)
       let response = await fetch(productsApiUrl)
       let data = await response.json()
-        setProducts(data)
+        setProducts(data) //just silence warning
         setFatching(false)
     }
     
   getProducts(productsApiUrl)
 
- }, [])
+ }, [setProducts])
   
- 
+  
   return (
   <div>
     <HeroSection />
+    <div className='category-section'>
+      {
+        categories.map((category,index) => {
+              let categoryClass = 'category'
+          if(category === currentCategory) {
+            categoryClass = 'category active-category'
+          }
+
+          return <span key={index} className={categoryClass} onClick={() =>setCurrentCategory(category)}>{category}</span>
+        })
+      }
+    </div>
     { fetching ? 
     <h1 className={isDarkMode ?"loading-data-text dark-color" : "loading-data-text"}> Loading . . .  </h1> :
     <SearchFilter 
@@ -47,12 +65,21 @@ const Home = () => {
               <p> No Products Found 🙄</p>
               </div>
             }
-
+            
+            
             return <div className="products-container">
-              {results.map((product => {
-                console.log(product.category);
-                
-                return <SingleProduct key={product.title} isDarkMode={isDarkMode} product={ product } cartProducts={cartProducts} setCartProducts={setCartProducts} showModel={showModel} setShowModel={setShowModel} modelProduct={modelProduct} setModelProduct={setModelProduct}/>
+              {results.map(((product) => {
+
+                if(currentCategory === "All") {
+                  return <SingleProduct key={product.title} isDarkMode={isDarkMode} product={ product } cartProducts={cartProducts} setCartProducts={setCartProducts} showModel={showModel} setShowModel={setShowModel} modelProduct={modelProduct} setModelProduct={setModelProduct}/>
+                }
+
+                else if(product.category === currentCategory) {
+                  return <SingleProduct key={product.title} isDarkMode={isDarkMode} product={ product } cartProducts={cartProducts} setCartProducts={setCartProducts} showModel={showModel} setShowModel={setShowModel} modelProduct={modelProduct} setModelProduct={setModelProduct}/>
+                }
+
+                return null
+              
            }))}
               <ContentModel />
             </div>
